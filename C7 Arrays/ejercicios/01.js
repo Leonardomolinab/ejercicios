4eb07a87612d38b36f150df678c83b0f14afbc91
leonardo <efrainleonardo.molinab@gmail.com>
2024-05-18 22:55:15 -0300

    modo original

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -1,7 +1,7 @@
 function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
+  
 }
 
 module.exports = devolverPrimerElemento;
- hola my framexrevia=saffsds
