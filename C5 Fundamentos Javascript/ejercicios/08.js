f741185a55eba3cff2f798e46e59d962c1836f62
leonardo <efrainleonardo.molinab@gmail.com>
2024-05-20 17:37:02 -0300

    resuelto ejercicos 5 primer parte

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -7,6 +7,12 @@ function esNumeroEntero(numero) {
   // -1212 ---> true 
   // 121.212 ---> false 
   // Tu código:
+if (typeof numero === 'number' && numero % 1 === 0) {
+  return true;
+} else {
+  return false;
+}
 }
 
-module.exports = esNumeroEntero;
+module.exports = esNumeroEntero;  
+//return Number.isInteger(numero);
